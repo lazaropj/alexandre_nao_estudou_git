4f62c0b58d58b9698f74498e2e2ea033675fabba
Lazaro Jr <linkout.lazaro@gmail.com>
2021-06-30 20:00:58 -0300

    commit de um arquivo q nao deveria existir

---
+++ vouvoltar.ts
@@ -0,0 +1,23 @@
+
+
+ajksdhjksdlfjhbh sdja
+'sdf'asl'dfas''
+f asd
+ fásd
+ fasd
+ fasd
+ f asd
+ f as
+ 'fsad
+ f
+ asf safdjk skflasdjlfasd''
+fasd 
+fsd f
+asdf
+as f
+asf
+asd fmasfasdfbl jkasdf adskfsdfsdfasd
+f as
+fasd
+f a
+sdf
